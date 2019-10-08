2fb2267b42d608d9fde4e5ad80054f1c64590324
Michael Rollberg <mrollberg@ymail.com>
2019-10-08 18:34:14 -0400

    Fixed api issue

--- PizzaStoreFrontEnd/src/app/service/google.service.ts
+++ PizzaStoreFrontEnd/src/app/service/google.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { googleConfig } from './googleConfig';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GoogleService {
 
-  constructor(private http: HttpClient, private googleConfig: googleConfig) { }
+  constructor(private http: HttpClient) { }
+
+  apiKey: string = "AIzaSyDTztgokgTSMkNvFVhnxkDLGbRwCH5oFik";
 
   addressToCoordinates(streetAddress: string = "", city: string = "", state: string = "", zipcode: number = 0): Observable<any> {
-    return this.http.get<any>(`https://maps.googleapis.com/maps/api/geocode/json?address=${streetAddress.replace(/ /g, "+")},+${city.replace(/ /g, "+")},+${state.replace(/ /g, "+")}+${zipcode}&key=${this.googleConfig.secretApiKey}`)
+    return this.http.get<any>(`https://maps.googleapis.com/maps/api/geocode/json?address=${streetAddress.replace(/ /g, "+")},+${city.replace(/ /g, "+")},+${state.replace(/ /g, "+")}+${zipcode}&key=${this.apiKey}`)
   }
 
   coordinatesToAddress(latitude: number = 0, longitude: number = 0): Observable<any> {
-    return this.http.get<any>(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${this.googleConfig.secretApiKey}`)
+    return this.http.get<any>(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${this.apiKey}`)
   }
 }
